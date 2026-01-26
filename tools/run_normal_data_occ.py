cf6e6d986ae323dd542903eea75eb8ca729d5122
huangbinling20555118 <binlinghuang3388@gmail.com>
2026-01-26 17:10:42 +0800

    feat:第2版重构化代码结构

---
+++ tools/run_normal_data_occ.py
@@ -0,0 +1,39 @@
+import os 
+import sys 
+import numpy as np
+import traceback
+from occ.base import DataGenerator
+sys.path.append('.') 
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.dirname(current_dir)
+sys.path.append(project_root)
+
+
+def run_normal_data_pipeline():
+    # ================= 1. 配置参数 =================
+    
+    # 数据集根目录 
+    input_path = "/mnt/data_ssd/yenianjin/project/Pi3/vis/real_0112_2/videos" 
+
+    # 视频文件名
+    video_name = "office.mp4"
+    
+    # 输出结果的根目录
+    save_dir = "/mnt/data_ssd/yenianjin/project/Pi3/vis/real_0112_2/videos/outputs"
+    
+    # 模型 Checkpoint 路径
+    model_dir = os.path.join(project_root, "ckpt")
+    
+    # 配置文件路径
+    config_path = os.path.join(project_root, "occ","configs", "config.yaml")
+
+    
+    # ================= 2. 初始化 =================
+    generator = DataGenerator(config_path, save_dir, model_dir)
+    
+    # ================= 3. 开始处理 =================
+    generator.occ_gen_all_pipeline(os.path.join(input_path, video_name), pcd_save=True)
+
+if __name__ == "__main__":
+    run_normal_data_pipeline()
